fdcca7c1b4e38ff47971b7c3e09b9cecd6442684
Kriss <geraltrivia19@gmail.com>
2022-03-14 13:15:35 +0100

    I added the return word to the beginning of the function names for better definition of the names.

--- output_messages.py
+++ output_messages.py
@@ -1,13 +1,13 @@
 from all_setting import TEXT_COLOR_FOR_USER, TEXT_COLOR_FOR_PROGRAMMER, TEXT_COLOR_MAIN_MENU
 
 
-def messages_for_player(message):
+def return_messages_for_player(message):
     return TEXT_COLOR_FOR_USER + message
 
 
-def messages_for_programmer(message):
+def return_messages_for_programmer(message):
     return TEXT_COLOR_FOR_PROGRAMMER + message
 
 
-def menu_text(menu_color_text):
+def return_menu_text(menu_color_text):
     return TEXT_COLOR_MAIN_MENU + menu_color_text
--- test/test_output_messages.py
+++ test/test_output_messages.py
@@ -11,8 +11,8 @@ for_test_color_text_for_main_menu = "\033[1;32m"
         ("Hello world", for_test_color_text_for_user + "Hello world"),
         ("Hello player one", for_test_color_text_for_user + "Hello player one")
 ))
-def test_messages_for_player(message, expected):
-    result = om.messages_for_player(message)
+def test_return_messages_for_player(message, expected):
+    result = om.return_messages_for_player(message)
     assert for_test_color_text_for_user == TEXT_COLOR_FOR_USER
     assert result == expected
 
@@ -21,8 +21,8 @@ def test_messages_for_player(message, expected):
         ("Warning", for_test_color_text_for_programmer + "Warning"),
         ("First problem", for_test_color_text_for_programmer + "First problem")
 ))
-def test_messages_for_programmer(message, expected):
-    result = om.messages_for_programmer(message)
+def test_return_messages_for_programmer(message, expected):
+    result = om.return_messages_for_programmer(message)
     assert for_test_color_text_for_programmer == TEXT_COLOR_FOR_PROGRAMMER
     assert result == expected
 
@@ -31,7 +31,7 @@ def test_messages_for_programmer(message, expected):
         ("1.Start game ", for_test_color_text_for_main_menu + "1.Start game "),
         ("3.Exit", for_test_color_text_for_main_menu + "3.Exit")
 ))
-def test_menu_text(message, expected):
-    result = om.menu_text(message)
+def test_return_menu_text(message, expected):
+    result = om.return_menu_text(message)
     assert for_test_color_text_for_main_menu == TEXT_COLOR_MAIN_MENU
     assert result == expected
